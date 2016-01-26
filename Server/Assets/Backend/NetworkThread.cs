e97254d20c231711a2b45841c1af3dcb08e96fba
ElderMayday <nerdy308deez44>
2016-01-26 23:05:47 +0300

    Preliminary version
    Transmission is working. Backend folder defined.

---
+++ Server/Assets/Backend/NetworkThread.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Backend
+{
+    public class NetworkThread
+    {
+        public string Data { get; private set; }
+        public float DataFloat { get; private set; }
+        public bool IsWorking { get; private set; }
+
+        protected Thread thread;
+
+        public NetworkThread()
+        {
+            thread = new Thread(doThread);
+            thread.Start();
+            IsWorking = false;
+        }
+
+        public void doThread()
+        {
+            while (true)
+            {
+                IPAddress ip = IPAddress.Parse("192.168.100.3");
+                IPEndPoint ipEndPoint = new IPEndPoint(ip, 11000);
+                Socket socketListener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                socketListener.Bind(ipEndPoint);
+                socketListener.Listen(10);
+                Socket socketHandler = socketListener.Accept();
+                IsWorking = true;
+                socketHandler.ReceiveTimeout = 500;
+
+                while (true)
+                {
+                    byte[] bytes = new byte[1024];
+                    int bytesLength = socketHandler.Receive(bytes);
+                    Data = Encoding.UTF8.GetString(bytes, 0, bytesLength);
+                    DataFloat = (float)Convert.ToDouble(Data);
+
+                    if (bytesLength == 0)
+                        break;
+                }
+
+                IsWorking = false;
+                socketListener.Close();
+                ipEndPoint = null;
+            }
+        }
+    }
+}
---
+++ Server/Assets/Scripts/ScriptCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+using Backend;
+
+public class ScriptCamera : MonoBehaviour
+{
+    private NetworkThread networkThread;
+
+    private float smooth = 2.0F;
+    private float tiltAngle = 30.0F;
+
+    void Start()
+    {
+        networkThread = new NetworkThread();
+    }
+
+    void Update()
+    {
+        float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+        float tiltAroundX = Input.GetAxis("Vertical") * tiltAngle;
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+        if (networkThread.IsWorking)
+            target = Quaternion.Euler(tiltAroundX, 0, -networkThread.DataFloat * 5);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+    }
+}
